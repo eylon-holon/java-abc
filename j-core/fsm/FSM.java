803107190f5f9417a113c40964719488e2530b44
gosha-smeh <gosha.smeh@gmail.com>
2025-01-03 02:46:33 +0200

    fsm is here

--- j-core/fsm/FSM.java
+++ j-core/fsm/FSM.java
@@ -1,82 +1,85 @@
-import java.util.*;
+import java.util.Arrays;
 
 class FSM {
-    public static class With {
-        public boolean log;
-        public boolean strict;
-
-        public With() {
-            log = false;
-            strict = false;
-        }
-
-        public With(boolean log, boolean strict) {
-            this.log = log;
-            this.strict = strict;
-        }
-
-        public With logs() { return new With(true, strict); }
-        public With strict() { return new With(log, true); }
-    }
-
-    static class Letter {
-        public int id;
-        public char name;
-    }
-
-    static class State {
+    public static class State {
         public int id;
         public String name;
         public boolean ok;
+
+        public State(int id, String name, boolean ok) {
+            this.id = id;
+            this.name = name;
+            this.ok = ok;
+        }
     }
 
-    static class Rule {
+    public static class Rule {
         public State from;
         public State to;
         public char[] chars;
+
+        public Rule(State from, State to, char[] chars) {
+            this.from = from;
+            this.to = to;
+            this.chars = chars;
+        }
     }
 
-    static class Def {
+    public static class Def {
         public State[] states;
         public char[] alefBet;
         public Rule[] rules;
+
+        public Def(State[] states, char[] alefBet, Rule[] rules) {
+            this.states = states;
+            this.alefBet = alefBet;
+            this.rules = rules;
+        }
     }
 
+    private String name;
     private Def def;
-    private With args;
     private int[] ch2at;
     private int[][] fsm;
     private int current;
 
-    public FSM(Def def, With args) {
+    private boolean traceInit = false;
+
+    private void print(String fmt, Object... args) {
+        System.out.println(String.format(fmt, args));
+    }
+
+    public FSM(String name, Def def) {
+        this.name = name;
         this.def = def;
-        this.args = args;
 
         ch2at = new int[Character.MAX_VALUE];
-        for (var rule: def.rules) {
-            for (int at = 0; at < rule.chars.length; at++) {
-                ch2at[rule.chars[at]] = at;
-            }
+        for (int at = 0; at < def.alefBet.length; at++) {
+            var ch = def.alefBet[at];
+            ch2at[ch] = at;
+            if (traceInit) print("ch2at['%s'] = %d  ==>  ch2at[%d] = %d", ch, at, (int) ch, at);
         }
 
         fsm = new int[def.states.length][def.alefBet.length];
+
+        for (var ln: fsm)
+            Arrays.fill(ln, -1);
+
         for (var rule: def.rules) {
             int from = rule.from.id;
             int to = rule.to.id;
             for (var ch: rule.chars) {
                 var at = ch2at[ch];
                 fsm[from][at] = to;
+                if (traceInit) print("fsm[%s]['%s'] = %s  ==>  fsm[%d][%d] = %d",
+                    rule.from.name, ch, rule.to.name, from, at, to);
             }
         }
     }
 
-    private boolean error(State in, String fmt) {
-        System.out.println("ERROR: " + fmt);
-        return false;
-    }
-
-    private void log(State from, State to, int at) {
-
+    private int error(State in, String msg) {
+        System.out.println(String.format("ERROR in %s: %s", in.name, msg));
+        return -1;
     }
 
     private int getAt(char ch) {
@@ -85,40 +88,102 @@ class FSM {
             if (ch == chars[i])
                 return i;
         }
-        return 0;
+        return -1;
     }
 
-    private boolean next(char ch) {
+    private void traceTransition(State from, State to, int at) {
+        print("  '%s': %s --> %s", def.alefBet[at], from.name, to.name);
+    }
+
+    private int next(char ch, boolean trace) {
         var from = def.states[current];
         assert from != null;
 
         var at = getAt(ch);
-        if (at == 0)
-            return error(from, "Can't find letter " + ch);
+        if (at == -1)
+            return error(from, String.format("Undefined transition '%s'", ch));
 
         var next = fsm[current][at];
-        if (next == 0)
-            return error(from, "Can't find next state " + next);
+        if (next == -1)
+            return error(from, String.format("No transition for '%s' is defined", ch));
 
         var to = def.states[next];
         assert to != null;
 
-        log(from, to, at);
-        return true;
+        if (trace)
+            traceTransition(from, to, at);
+
+        return next;
+    }
+
+    public boolean accept(String word, boolean trace) {
+        if (trace)
+            print("FSM['%s']: accepting word '%s'", name, word);
+
+        current = 0;
+        for (var ch: word.toCharArray()) {
+            current = next(ch, trace);
+            if (current == -1)
+                return false;
+        }
+
+        var ok = def.states[current].ok;
+
+        if (trace)
+            print("FSM['%s']: '%s' is %s", name, word, ok ? "accepted" : "wrong");
+
+        return ok;
     }
 
     public boolean accept(String word) {
-        current = 0;
-        for (var ch: word.toCharArray()) {
-            if (!next(ch))
-                return false;
-        }
-        var state = getState(current);
-        return state.ok;
+        return accept(word, false);
     }
 
     @Override
     public String toString() {
-        return "FSM(...)";
+        var sb = new StringBuilder();
+
+        sb
+            .append(String.format(
+            "FSM['%s', %d, %d, %d]", name, def.states.length, def.alefBet.length, def.rules.length))
+            .append("\n");
+
+        sb
+            .append("  states:  [")
+            .append(String.join(", ", Arrays
+                .stream(def.states)
+                .map(st -> String.format("%s(%d)", st.name, st.id))
+                .toList()))
+            .append("]\n");
+
+        sb
+            .append("  alefBet: ")
+            .append(Arrays.toString(def.alefBet))
+            .append("\n");
+
+        sb.append("  rules:  [\n");
+        for (var rule: def.rules) {
+            sb.append(String.format("    %s -%s-> %s\n",
+                rule.from.name, Arrays.toString(rule.chars), rule.to.name));
+        }
+        sb.append("  ]\n");
+
+        sb.append("  fsm:  [\n");
+        for (int ln = 0; ln < fsm.length; ln++) {
+            sb.append(String.format("    %s [", def.states[ln].name));
+            var first = true;
+            for (int at = 0; at < fsm[ln].length; at++) {
+                if (!first)
+                    sb.append(", ");
+                var target = fsm[ln][at] >= 0 ? def.states[fsm[ln][at]].name : "XX";
+                sb.append(String.format("'%s'-> %s", 
+                    def.alefBet[at], target));
+                first = false;
+            }
+            sb.append("]\n");
+        }
+        sb.append("  ]");
+
+        return sb.toString();
     }
 }
--- j-core/fsm/FsmFromMermaid.java
+++ j-core/fsm/FsmFromMermaid.java
@@ -1,7 +1,21 @@
 class FsmFromMermaid {
     public static FSM parse(String name, String file) {
-        var args = new FSM.With();
-        var fsm = new FSM(args);
+        var q0 = new FSM.State(0, "q0", false);
+        var q1 = new FSM.State(1, "q1", false);
+        var q2 = new FSM.State(2, "q2", true);
+
+        var r0 = new FSM.Rule(q0, q1, new char[] {'0', '1'});
+        var r1 = new FSM.Rule(q1, q0, new char[] {'0'});
+        var r2 = new FSM.Rule(q1, q2, new char[] {'1'});
+
+        var def = new FSM.Def(
+            new FSM.State[] {q0, q1, q2},
+            new char[] {'0', '1'},
+            new FSM.Rule[] {r0, r1, r2}
+        );
+
+        var fsm = new FSM(name, def);
+
         return fsm;
     }    
 }
