75e6c1352e29ea06314af0d0394a6554feaa300d
Carmel Dayan <carmel.dayan@gebemot.com>
2025-02-03 10:41:15 +0000

    auto commit...

--- 07-queue-and-stack/Queue.java
+++ 07-queue-and-stack/Queue.java
@@ -2,17 +2,31 @@ class Queue<T> {
     private Node<T> head;
 
     public Queue() {
+        this.head=null;
     }
 
     public boolean isEmpty() {
+        if (this.head == null)
+        return true;
+        else
         return false;
+        
     }
 
-    public T head() {
-        return null;
-    }
+   
 
     public void insert(T value) {
+      int a=new int [this.head.length()+1];
+      Node node=null;
+      int count=0;
+      while (this.head!=null){
+      a[count]=this.head.getValue;
+      count++;
+      this.head=this.head.getNext;
+      }
+      a[this.head.length()+1]=value;
+      
+
     }
 
     public T remove() {
