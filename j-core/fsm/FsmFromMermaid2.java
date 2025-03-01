e1716657f836cf8f032da9228827ddc35aa20c2e
gosha-smeh <gosha.smeh@gmail.com>
2025-03-01 05:29:53 +0200

    fsm with stack is started

--- j-core/fsm/FsmFromMermaid2.java
+++ j-core/fsm/FsmFromMermaid2.java
@@ -1,9 +1,16 @@
 class FsmFromMermaid2 {
+    private static void print(String fmt, Object... args) {
+        System.out.println(String.format(fmt, args));
+    }
+
     public static IFSM parse(String graphName, String filePath, boolean... logFlags) {
         var log = logFlags.length > 0 && logFlags[0];
 
         var graph = MermaidFile2.loadGraph(graphName, filePath, log);
+        if (log) print("graph '%s': loaded", graph.name);
+
         var parser = new FsmParser(graph.lines, log);
+        if (log) print("graph '%s': parsed", graph.name);
 
         var def = new FSM.Def(
             parser.getStates(),
@@ -11,6 +18,8 @@ class FsmFromMermaid2 {
             parser.getTransitions(),
             graph.props
         );
+
+        if (log) print("graph '%s': ready for fsm", graph.name);
 
         return graph.props.nondetermenistic() ?
             new NondetermenisticFsm(graphName, def, log) : 
